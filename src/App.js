752bc940bbbce9d2d7ed4987db643119ad18a069
Tyler Hou <tylerhou@google.com>
2018-12-05 18:30:22 -0800

    Add Apache license and code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import FormsList from "./pages/FormsList";
 import Eligibility from "./pages/Eligibility";
 import DefendantForm from "./pages/DefendantForm";
 import PlaintiffForm from "./pages/PlaintiffForm";
-import protectiveorder from './pages/protectiveorder';
-import affadavit from './containers/affadavit';
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import protectiveorder from "./pages/protectiveorder";
+import affadavit from "./containers/affadavit";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 0
+    };
+  }
+
+  componentDidMount() {
+    // this.interval = setInterval(
+    //   () => this.setState({ timer: this.state.timer + 1 }),
+    //   1000
+    // );
+  }
+
+  componentDidUnmount() {
+    //   clearInterval(this.interval);
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/list" component={FormsList} />
-          <Route exact path="/eligibility" component={Eligibility} />
-          <Route exact path="/defendant" component={DefendantForm} /> 
-          <Route exact path="/plaintiff" component={PlaintiffForm} /> 
-          <Route exact path="/protectiveorder" component={protectiveorder} /> 
-          <Route exact path="/affadavit" component={affadavit} /> 
+          <Route
+            exact
+            path="/eligibility"
+            render={() => (
+              <Eligibility
+                title={
+                  "this page has been loaded for " +
+                  this.state.timer +
+                  " seconds"
+                }
+              />
+            )}
+          />
+          <Route exact path="/defendant" component={DefendantForm} />
+          <Route exact path="/plaintiff" component={PlaintiffForm} />
+          <Route exact path="/protectiveorder" component={protectiveorder} />
+          <Route exact path="/affadavit" component={affadavit} />
         </Switch>
       </Router>
     );
--- src/pages/Eligibility/index.js
+++ src/pages/Eligibility/index.js
@@ -1,90 +1,133 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 import { Button, Span } from "./styles";
 import { SSL_OP_TLS_BLOCK_PADDING_BUG } from "constants";
 
+const background = {
+  backgroundImage:
+    "url('https://images.unsplash.com/photo-1494451930944-8998635c2123?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a6fc9d727accaab5052cd754f634e768&auto=format&fit=crop&w=1651&q=80')"
+};
+const letterStyle = {
+  margin: 50,
+  display: "inline-block",
+  fontFamily: "sans-serif",
+  fontSize: 25,
+  color: "#FFFFFF",
+  textAlign: "left"
+};
+const buttonstyle = {
+  padding: 50
+};
 
 class FormsPage extends React.Component {
-    constructor() {
-      super();
-      this.state = {isChecked: false};
-    this.handleChecked = this.handleChecked.bind(this); // set this, because you need get methods from CheckBox 
-    }
-    handleChecked () {
-      this.setState({isChecked: !this.state.isChecked});
-    }
-  
-    handleChange(event) {
-      this.setState({title: event.target.value})
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();  
-      var checks = document.getElementsByClassName('checkbox');
-      var count = 0;
-        for (var i = 0;i<3;i++){
-          var link;
-          if (checks[i].checked===true){
-            count=count+1;
-            }
-          }
-          if (count!==3){
-            // link= "http://localhost:3003/";
-            alert('Unfortunately, you are not eligible for a plaintiff form.Please click non-eligible to access other resources');
-            // return link;
-          }
-          else{
-            // link="http://localhost:3003/list";
-            alert('done. Click "Eligible" to acces the next form');
-            // return link;
-          }
-        }
+  constructor() {
+    super();
+    this.state = {
+      wasMarried: false,
+      hasBeenThreatened: false,
+      inSameAreaAsCourt: false
+    };
+    // set this, because you need get methods from CheckBox
+    this.handleWasMarriedChecked = this.handleWasMarriedChecked.bind(this);
+    this.handleHasBeenThreatenedChecked = this.handleHasBeenThreatenedChecked.bind(
+      this
+    );
+    this.handleSameAreaChecked = this.handleSameAreaChecked.bind(this);
+  }
+  handleWasMarriedChecked(e) {
+    this.setState({ wasMarried: e.target.checked });
+  }
 
-    render() {
-      var background = {
-        backgroundImage: "url('https://images.unsplash.com/photo-1494451930944-8998635c2123?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a6fc9d727accaab5052cd754f634e768&auto=format&fit=crop&w=1651&q=80')",
-      }
-      var letterStyle = {
-        margin: 50,
-        display: "inline-block",
-        fontFamily: "sans-serif",
-        fontSize: 25,
-        color: "#FFFFFF",
-        textAlign: "left"
-      };
-      var buttonstyle = {
-        padding: 50,
-      }
-    var count = 0;
-    if (this.state.isChecked) {
-      count=count+1
+  handleHasBeenThreatenedChecked(e) {
+    this.setState({ hasBeenThreatened: e.target.checked });
+  }
+
+  handleSameAreaChecked(e) {
+    this.setState({ inSameAreaAsCourt: e.target.checked });
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (
+      this.state.wasMarried &&
+      this.state.hasBeenThreatened &&
+      this.state.inSameAreaAsCourt
+    ) {
+      this.props.history.push("/defendant");
     } else {
-      count=count
-    }
-      return (
-        <form onSubmit={this.handleSubmit} style = {background}>
-        <div  style={letterStyle}>
-        <h2>Protective Order Elgibility Confirmation</h2>
-          <table>
-              <td>
-                <p>Were you and your abuser married, residing together, in a substantive dating or engagement relationship, related by blood or marriage, or have a child in common?</p><input type="checkbox" class="checkbox" name="q1" value="Test1" onChange={ this.handleChecked }></input>
-                <p>Are you suffering from abuse because your abuser has harmed or attempted to harm you physically, or put you in fear of imminent serious physical harm, or caused you to engage in sexual relations involuntarily by using force, threat or duress?</p><input type="checkbox" class="checkbox" name="q2" value="Test2"></input>
-                <p>Are you currently living within the geographical area of this court, or used to live within the geographical area of this court but you left to avoid abuse? </p><input type="checkbox" class="checkbox" name="q3" value="Test3" onclick={this.handleSubmit}></input>
-                </td>
-             </table>
-             </div>
-          <div style={buttonstyle}>
-            <Button className="btn btn-default" type="submit" >Check</Button> 
-           <Link to="/list">
-           <Button> I am eligible</Button>
-            </Link>
-            <Link to="/">
-            <Button>I am not eligible</Button>
-          </Link>
-          </div>
-          </form>
+      alert(
+        "Unfortunately, you are not eligible for a plaintiff form.Please click non-eligible to access other resources"
       );
     }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} style={background}>
+        <div style={letterStyle}>
+          <h2>Protective Order Elgibility Confirmation</h2>
+          <h3>{this.props.title}</h3>
+          <table>
+            <td>
+              <p>
+                Were you and your abuser married, residing together, in a
+                substantive dating or engagement relationship, related by blood
+                or marriage, or have a child in common?
+              </p>
+              <input
+                type="checkbox"
+                class="checkbox"
+                name="q1"
+                checked={this.state.wasMarried}
+                onChange={this.handleWasMarriedChecked}
+              />
+              <p>
+                Are you suffering from abuse because your abuser has harmed or
+                attempted to harm you physically, or put you in fear of imminent
+                serious physical harm, or caused you to engage in sexual
+                relations involuntarily by using force, threat or duress?
+              </p>
+              <input
+                type="checkbox"
+                class="checkbox"
+                name="q2"
+                checked={this.state.hasBeenThreatened}
+                onChange={this.handleHasBeenThreatenedChecked}
+              />
+              <p>
+                Are you currently living within the geographical area of this
+                court, or used to live within the geographical area of this
+                court but you left to avoid abuse?{" "}
+              </p>
+              <input
+                type="checkbox"
+                class="checkbox"
+                name="q3"
+                checked={this.state.inSameAreaAsCourt}
+                onChange={this.handleSameAreaChecked}
+              />
+            </td>
+          </table>
+        </div>
+        <div style={buttonstyle}>
+          <Button className="btn btn-default" type="submit">
+            Check
+          </Button>
+          <Link to="/list">
+            <Button> I am eligible</Button>
+          </Link>
+          <Link to="/">
+            <Button>I am not eligible</Button>
+          </Link>
+        </div>
+      </form>
+    );
   }
-export default FormsPage;
+}
+export default withRouter(FormsPage);
